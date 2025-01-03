01152249ec54ee93fd695e708a44e7730895df76
Pukar77 <pukarrimal11@gmail.com>
2025-01-03 07:15:51 +0545

    Add user edit functionality for email and username with form updates

--- Client/vite-project/src/pages/Edit.jsx
+++ Client/vite-project/src/pages/Edit.jsx
@@ -3,6 +3,8 @@ import Navbar from "../navbar/Navbar";
 
 function Edit() {
   const [userinfo, setUserInfo] = useState([]);
+  const [email, setEmail] = useState(" ");
+  const [username, setUsername] = useState(" ");
 
   const gettoken = () => {
     return localStorage.getItem("accesstoken");
@@ -33,6 +35,31 @@ function Edit() {
     }
   };
 
+  const edituser = async (id) => {
+    try {
+      const formdata = new FormData();
+      formdata.append("username", username);
+      formdata.append("email", email);
+
+      alert(id);
+      let response = await fetch(
+        `http://localhost:8000/api/edituser/update/${id}`,
+        {
+          method: "put",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          body: formdata
+        }
+      );
+      let data = await response.json();
+      console.log(data);
+      setEditdata(data);
+    } catch (e) {
+      console.log("Some error occured", e);
+    }
+  };
+
   useEffect(() => {
     userdetail();
   }, []);
@@ -50,6 +77,10 @@ function Edit() {
             class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
             required
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
           />
           <label
             for="floating_email"
@@ -69,6 +100,10 @@ function Edit() {
               class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
               placeholder=" "
               required
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
             />
             <label
               for="floating_first_name"
@@ -78,46 +113,15 @@ function Edit() {
             </label>
           </div>
         </div>
-        <div class="grid md:grid-cols-2 md:gap-6">
-          <div class="relative z-0 w-full mb-5 group">
-            <input
-              type="tel"
-              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-              name="floating_phone"
-              id="floating_phone"
-              class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
-              required
-            />
-            <label
-              for="floating_phone"
-              class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Phone number (123-456-7890)
-            </label>
-          </div>
-          <div class="relative z-0 w-full mb-5 group">
-            <input
-              type="text"
-              name="floating_company"
-              id="floating_company"
-              class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
-              required
-            />
-            <label
-              for="floating_company"
-              class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Company (Ex. Google)
-            </label>
-          </div>
-        </div>
+        <div class="grid md:grid-cols-2 md:gap-6"></div>
         <button
+          onClick={() => {
+            edituser(userinfo.userid);
+          }}
           type="submit"
           class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          Submit
+          Submit and Edit
         </button>
       </form>
     </div>
